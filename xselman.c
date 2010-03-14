bf5711d70b2380ca585fd57ca84d79259b66cf1b
Leandro Lisboa Penz <llpenz@gmail.com>
2010-03-14 17:58:07 -0300

    xselman works!

--- xselman.c
+++ xselman.c
@@ -1,99 +1,99 @@
 
 #include <gtk/gtk.h>
 #include <string.h>
+#include <stdlib.h>
 
-void on_window_destroy(GtkObject * object, gpointer user_data)
+#define XSEL_NUM ((sizeof xsel_info)/(sizeof(*xsel_info)))
+#define XSELMAN_INTERVAL 1500
+
+
+const struct {
+	const char *name;
+	GdkAtom cba;
+}
+xsel_info[] = {
+	{ "primary"   , GDK_SELECTION_PRIMARY   },
+	{ "secondary" , GDK_SELECTION_SECONDARY },
+	{ "clipboard" , GDK_SELECTION_CLIPBOARD },
+};
+
+typedef struct _xselman_t xselman_t;
+
+typedef struct
 {
-	gtk_main_quit();
+	int i;
+	char name[20];
+	GtkClipboard *cb;
+	char *txt;
+	xselman_t *xselman;
+}
+xsel_t;
+
+struct _xselman_t
+{
+	GtkWidget *w;
+	xsel_t xsel[XSEL_NUM];
+};
+
+/****************************************************************************/
+
+void xsel_check_txt(GtkClipboard *clipboard, const gchar *text, gpointer data)
+{
+	xsel_t *xsel = data;
+	int i;
+
+	if (! xsel->txt) {
+		if (text)
+			xsel->txt = strdup(text);
+		return;
+	}
+
+	if (strcmp(text, xsel->txt) == 0)
+		return;
+
+	printf("xsel %-10s: [%s] -> [%s]\n", xsel->name, xsel->txt, text);
+	for (i = 0; i < XSEL_NUM; i++) {
+		xsel_t *xseli = &xsel->xselman->xsel[i];
+
+		free(xseli->txt);
+		xseli->txt = strdup(text);
+
+		if (i == xsel->i)
+			continue;
+
+		gtk_clipboard_set_text(xseli->cb, text, strlen(text));
+	}
 }
 
-char *old_clipboard = NULL;
 
-void selection_received(GtkWidget *window, GdkEvent *event)
+int xsel_check(gpointer data)
 {
-	char *buf;
-	GdkAtom atom;
-	gint format, length;
+	xsel_t *xsel = data;
 
-	printf("selection received\n");
+	gtk_clipboard_request_text(xsel->cb, xsel_check_txt, xsel);
+	g_timeout_add(XSELMAN_INTERVAL, xsel_check, xsel);
 
-	length = gdk_selection_property_get(window->window, (guchar **)&buf, &atom, &format);
-
-	if (old_clipboard && !strcmp(old_clipboard, buf)) {
-		g_free(buf);
-	}
-	else {
-		char *b = buf;
-		char *c;
-		while (b && (c = strstr(b, "http://")))
-		{
-			char *p = strpbrk(c, " \t\r\n");
-			char *url = p ? g_strndup(c, p - c) : g_strdup(c);
-			puts(url); // handle url
-			g_free(url);
-			b = c + 1;
-		}
-		old_clipboard = buf;
-	}
-}
-
-int clipboard_timeout(void *window)
-{
-	//cb = gtk_clipboard_get(GDK_SELECTION_PRIMARY);
-	printf("clipboard timeout\n");
-	//gtk_selection_convert(GTK_WIDGET(window), GDK_SELECTION_CLIPBOARD, GDK_TARGET_STRING, GDK_CURRENT_TIME);
-	//g_timeout_add(250, clipboard_timeout, window);
 	return 0;
 }
 
-#if 0
-int main(int argc, char *argv[])
-{
-	GtkBuilder *builder;
-
-	GtkWidget *window;
-
-	gtk_init(&argc, &argv);
-
-	builder = gtk_builder_new();
-	gtk_builder_add_from_file(builder, "cm.ui", NULL);
-
-	window = GTK_WIDGET(gtk_builder_get_object(builder, "window1"));
-	gtk_builder_connect_signals(builder, NULL);
-	g_object_unref(G_OBJECT(builder));
-
-	gtk_widget_show(window);
-
-	// Monitor clipboard
-	g_signal_connect(G_OBJECT(window), "selection-received", G_CALLBACK(selection_received), NULL);
-	//g_timeout_add(250, clipboard_timeout, window);
-
-	gtk_main();
-
-	return 0;
-}
-#endif
-
-void clipboard_recv(GtkClipboard *clipboard, const gchar *text, gpointer data)
-{
-	printf("aqui: [%s]\n", text);
-	gtk_clipboard_request_text(gtk_clipboard_get(GDK_SELECTION_PRIMARY)
-			, clipboard_recv, NULL);
-}
 
 int main(int argc, char *argv[])
 {
-	GtkWidget *top;
+	xselman_t xselman;
+	int i;
 
 	gtk_init(&argc, &argv);
 
-	top = gtk_invisible_new();
+	xselman.w = gtk_invisible_new();
 
-	// Monitor clipboard
-	//g_signal_connect(G_OBJECT(top), "selection-received", G_CALLBACK(selection_received), NULL);
-	gtk_clipboard_request_text(gtk_clipboard_get(GDK_SELECTION_PRIMARY)
-			, clipboard_recv, NULL);
-	//g_timeout_add(250, clipboard_timeout, top);
+	for (i = 0; i < XSEL_NUM; i++) {
+		xselman.xsel[i].i = i;
+		snprintf(xselman.xsel[i].name, sizeof(xselman.xsel[i].name), "%s", xsel_info[i].name);
+		xselman.xsel[i].cb = gtk_clipboard_get(xsel_info[i].cba);
+		xselman.xsel[i].txt = NULL;
+		xselman.xsel[i].xselman = &xselman;
+		g_timeout_add(XSELMAN_INTERVAL, xsel_check, &xselman.xsel[i]);
+	}
 
 	gtk_main();
 
